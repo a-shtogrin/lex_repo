e0c980ea3fb1308133b6ceec1d64c4a682f53fbb
Alexey Shtogrin <lex_dobr@mail.ru>
2022-09-24 10:29:34 +0300

    first commit

---
+++ exemple.js
@@ -0,0 +1,16 @@
+//let
+
+let a = 42
+
+if (true) {
+    let a = 55
+
+    console.log("uu", a)
+}
+
+console.log(a)
+
+//const
+
+const color = '#ffeedd'
+
---
+++ index.js
@@ -0,0 +1,17 @@
+console.log("Hello world!");
+
+const ourPlanetName = "Земля";
+let userCurrentTime= "22";
+let articleCount = "10";
+let isCash = "Да";
+const userFirstName = "Lex";
+const userSurname = "Sh";
+const userSecondName = "Ivan";
+
+console.log (ourPlanetName);
+console.log (userCurrentTime);
+console.log (articleCount);
+console.log (isCash);
+console.log (userFirstName);
+console.log (userSurname);
+console.log (userSecondName)
