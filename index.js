88929af5015fb46f75bacb6db7ada0421de0d63f
Alexey Shtogrin <lex_dobr@mail.ru>
2022-09-24 12:48:51 +0300

    Изменения 3

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
-console.log("Hello world!");
+// console.log("Hello world!");
 
-const ourPlanetName = "Земля";
-let userCurrentTime= "22";
-let articleCount = "10";
-let isCash = "Да";
-const userFirstName = "Lex";
-const userSurname = "Sh";
-const userSecondName = "Ivan";
+// const ourPlanetName = "Земля";
+// let userCurrentTime = "22";
+// let articleCount = "10";
+// let isCash = "Да";
+// const userFirstName = "Lex";
+// const userSurname = "Sh";
+// const userSecondName = "Ivan";
 
-console.log (ourPlanetName);
-console.log (userCurrentTime);
-console.log (articleCount);
-console.log (isCash);
-console.log (userFirstName);
-console.log (userSurname);
-console.log (userSecondName)
+// console.log(ourPlanetName);
+// console.log(userCurrentTime);
+// console.log(articleCount);
+// console.log(isCash);
+// console.log(userFirstName);
+// console.log(userSurname);
+// console.log(userSecondName);
+
+// //const age = 18;
+// //const userData = `Имя пользователя ${userFirstName}, возраст ${age}`;
+// //console.log(userData);
+
+// const isUser = false;
+// const isVisible = true;
+
+let art = null;
+// console.log(art);
+
+// let count;
+// console.log(count);
+
+let userData = {
+  name: "Ivan",
+  age: "34",
+};
+console.log(userData);
+console.log(userData.name);
+
+console.log(typeof userData);
+console.log(typeof userData.name);
+
+//alert("Panic") - для отображения в браузере.
+
+console.log(typeof art)  //-  исключениеб тип данных null определяется с помощью  typeof как object!!!
+
+
