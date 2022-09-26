993d380813f45b8719cf40cec691f2f266ad945b
Alexey Shtogrin <lex_dobr@mail.ru>
2022-09-26 19:44:44 +0300

    шяь

--- exemple.js
+++ exemple.js
@@ -1,16 +1,18 @@
 //let
 
-let a = 42
+let a = 42;
 
 if (true) {
-    let a = 55
+  let a = 55;
 
-    console.log("uu", a)
+  console.log("uu", a);
 }
 
-console.log(a)
+console.log(a);
 
 //const
 
-const color = '#ffeedd'
+const color = "#ffeedd";
 
+let name = "Il";
+console.log(`hello ${"name"}`)
