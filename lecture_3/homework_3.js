3a8f9c96d900728455d2e013f53eebc658736ba2
Alexey Shtogrin <lex_dobr@mail.ru>
2022-10-08 08:48:25 +0300

    домашняя работа 3

---
+++ lecture_3/homework_3.js
@@ -0,0 +1,21 @@
+console.log("1.", "кот" > "код"); //true
+console.log("2.", "2" + 2 * "2" );  //24
+console.log("3.", undefined == null );  //true
+console.log("4.", undefined != null);  // false
+console.log("5.", null == 0);  //true -неверно!  д.б false (NaN не равно 0)
+console.log("6.", 2 > "3");  //false
+console.log("7.", null - false + true );  //1
+console.log("8.", 1 / "l");  // NaN
+console.log("9.", "2" * "3");  //6
+console.log("10.", 4 + 5 + "O");  //90
+console.log("11.", "l" + 4 + 5);  //l45
+console.log("12.", "4" - 2);  // 2
+console.log("13.","4" - "4x"); //NaN
+console.log("14.", "23" == 23);  // true
+console.log("15.", null == false);  //true -неверно!  д.б false  (NaN не равно 0)
+console.log("16.", " -4 "/ 0 + 1);  //-infinity
+console.log("17.", null + 1);  //1
+console.log("18.", undefined + null);  //NaN
+console.log("19.", 1 === "1")  //false
+console.log("20.", "2" > 10)  //false
+console.log("21.", NaN == undefined)  //false
