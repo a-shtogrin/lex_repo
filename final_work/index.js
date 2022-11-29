442e8e1e6ba864deabc49eb04ee4db06f6c51781
DESKTOP-1J2M7HO\Алексей <lex_dobr@mail.ru>
2022-11-29 19:35:55 +0300

    аши

---
+++ final_work/index.js
@@ -0,0 +1,6 @@
+
+// создание header
+
+let createNode = body.createElement ('header')
+createNode.className = "alert";
+createNode.innerHTML = "<strong>Всем привет!</strong> Вы прочитали важное сообщение.";
